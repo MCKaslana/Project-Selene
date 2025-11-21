6a180777e3ba81033953c52f9323bf5eb38d9690
William <quagsire089@gmail.com>
2025-11-20 19:53:12 -0700

    Added DOtween and ButtonAnim
    Added basic button animations with DOTween

---
+++ Assets/UIAnimations/ButtonAnim.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public enum ButtonMode
+{
+    Default,
+    Slidein
+}
+
+public class ButtonAnim : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [Header("Button Settings")]
+    [SerializeField] private ButtonMode _mode;
+
+    private Vector2 _originalX;
+    private Vector2 _originalY;
+
+    private void Awake()
+    {
+        _originalX.x = transform.localPosition.x;
+        _originalY.y = transform.localPosition.y;
+    }
+
+    private void OnEnable()
+    {
+        if (_mode == ButtonMode.Slidein) transform.DOLocalMoveX(0f, 0.7f);
+    }
+
+    private void OnDisable()
+    {
+        if (_mode == ButtonMode.Slidein)
+        {
+            transform.localPosition = new Vector2(_originalX.x, _originalY.y);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(1.15f, 0.5f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(1f, 0.5f);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.DOScale(1f, 0.5f);
+    }
+}
