95a5af9160d169e888df52439b863fd5834989e3
William <quagsire089@gmail.com>
2025-11-25 20:53:18 -0700

    Added difficulty SO

---
+++ Assets/Scripts/DifficultyScripts/DifficultyData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DifficultyData", menuName = "Difficulties/DifficultyData")]
+public class DifficultyData : ScriptableObject
+{
+    public ShapeKeyObject[] shapeKeys;
+}
