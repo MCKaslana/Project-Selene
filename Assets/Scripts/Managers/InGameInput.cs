3bdaa1bcc697e6e8ace1b4417616f513d956d490
William <quagsire089@gmail.com>
2025-12-04 19:57:03 -0700

    Added separated input handling
    Fixed data leak and game can be fully replayable

---
+++ Assets/Scripts/Managers/InGameInput.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class InGameInput : MonoBehaviour
+{
+    [Header("Input Reference")]
+    private InputManager _input;
+
+    [Header("ShapeKeys")]
+    [SerializeField] private List<ShapeKeyObject> _shapeKeys;
+    [SerializeField] private Transform _shapeKeySpawn;
+    [SerializeField] private GameObject _pauseMenu;
+
+    private ShapeKeyObject _currentShapeKey;
+
+    private int _activeShapeKeyIndex = 0;
+
+    private void Awake()
+    {
+        _input = GetComponent<InputManager>();
+    }
+
+    private void OnEnable()
+    {
+        _input.OnTryHitNote += TryHitNote;
+        _input.OnKeyOnePress += () => ToggleShapeKey(0);
+        _input.OnKeyTwoPress += () => ToggleShapeKey(1);
+        _input.OnKeyThreePress += () => ToggleShapeKey(2);
+    }
+
+    private void OnDisable()
+    {
+        _input.OnTryHitNote -= TryHitNote;
+        _input.OnKeyOnePress -= () => ToggleShapeKey(0);
+        _input.OnKeyTwoPress -= () => ToggleShapeKey(1);
+        _input.OnKeyThreePress -= () => ToggleShapeKey(2);
+    }
+
+    private void Start()
+    {
+        foreach (var key in _shapeKeys)
+        {
+            GameObject obj = Instantiate(key.keyObject, _shapeKeySpawn.position, Quaternion.identity, _shapeKeySpawn);
+            key.runTimeObject = obj;
+            obj.SetActive(false);
+        }
+
+        ToggleShapeKey(0);
+    }
+
+    private void TogglePauseMenu()
+    {
+        if (_pauseMenu != null)
+        {
+            bool isActive = _pauseMenu.activeSelf;
+            _pauseMenu.SetActive(!isActive);
+            Time.timeScale = isActive ? 1f : 0f;
+        }
+    }
+
+    private void ToggleShapeKey(int index)
+    {
+        if (index < 0 || index >= _shapeKeys.Count)
+            return;
+
+        _activeShapeKeyIndex = index;
+        _currentShapeKey = _shapeKeys[index];
+
+        for (int i = 0; i < _shapeKeys.Count; i++)
+        {
+            var key = _shapeKeys[i];
+            bool isActive = i == index;
+            if (key.runTimeObject != null)
+                key.runTimeObject.SetActive(isActive);
+        }
+    }
+
+    private void TryHitNote()
+    {
+        Debug.Log("HitNote");
+
+        NoteControl[] notes = FindObjectsByType<NoteControl>(FindObjectsSortMode.None);
+        if (notes.Length == 0) return;
+
+        float currentTime = SongManager.Instance.GetSongTime();
+
+        NoteControl closest = notes
+            .Where(n => !n.IsHit())
+            .OrderBy(n => Mathf.Abs(currentTime - n.GetTargetTime()))
+            .FirstOrDefault();
+
+        if (closest != null)
+        {
+            bool hit = closest.TryHit(_currentShapeKey.keyValue);
+
+            if (!hit)
+                SongManager.Instance.RegisterHit(new MissJudgement());
+        }
+    }
+}
--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -3,101 +3,55 @@ using System.Linq;
 using System.Collections.Generic;
 using System;
 using UnityEditor;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     private PlayerInputs _playerInput;
 
-    [Header("ShapeKeys")]
-    [SerializeField] private List<ShapeKeyObject> _shapeKeys;
-    [SerializeField] private Transform _shapeKeySpawn;
-    [SerializeField] private GameObject _pauseMenu;
-
-    private ShapeKeyObject _currentShapeKey;
-
-    private int _activeShapeKeyIndex = 0;
+    public event Action OnTryHitNote;
+    public event Action OnKeyOnePress;
+    public event Action OnKeyTwoPress;
+    public event Action OnKeyThreePress;
 
     private void Awake()
     {
         _playerInput = new PlayerInputs();
 
-        _playerInput.Player.ClickNote.performed
-            += context => TryHitNote();
-
-        _playerInput.Player.SwitchKey1.performed
-            += context => ToggleShapeKey(0);
-        _playerInput.Player.SwitchKey2.performed
-            += context => ToggleShapeKey(1);
-        _playerInput.Player.SwitchKey3.performed
-            += context => ToggleShapeKey(2);
-
-        _playerInput.Player.Pause.performed
-            += context => TogglePauseMenu();
-
-        _playerInput.Player.Enable();
-
-        _pauseMenu.SetActive(false);
+        _playerInput.Enable();
     }
 
-    private void Start()
+    private void OnEnable()
     {
-        foreach (var key in _shapeKeys)
-        {
-            GameObject obj = Instantiate(key.keyObject, _shapeKeySpawn.position, Quaternion.identity, _shapeKeySpawn);
-            key.runTimeObject = obj;
-            obj.SetActive(false);
-        }
+        var player = _playerInput.Player;
 
-        ToggleShapeKey(0);
+        player.ClickNote.performed += TryHitPerformed;
+        player.SwitchKey1.performed += SwitchOnePerformed;
+        player.SwitchKey2.performed += SwitchTwoPerformed;
+        player.SwitchKey3.performed += SwitchThreePerformed;
     }
 
-    private void TogglePauseMenu()
+    private void OnDisable()
     {
-        if (_pauseMenu != null)
-        {
-            bool isActive = _pauseMenu.activeSelf;
-            _pauseMenu.SetActive(!isActive);
-            Time.timeScale = isActive ? 1f : 0f;
-        }
+        var player = _playerInput.Player;
+
+        player.ClickNote.performed -= TryHitPerformed;
+        player.SwitchKey1.performed -= SwitchOnePerformed;
+        player.SwitchKey2.performed -= SwitchTwoPerformed;
+        player.SwitchKey3.performed -= SwitchThreePerformed;
+
+        _playerInput.Disable();
     }
 
-    private void ToggleShapeKey(int index)
-    {
-        if (index < 0 || index >= _shapeKeys.Count)
-            return;
+    private void TryHitPerformed(InputAction.CallbackContext ctx)
+        => OnTryHitNote?.Invoke();
 
-        _activeShapeKeyIndex = index;
-        _currentShapeKey = _shapeKeys[index];
+    private void SwitchOnePerformed(InputAction.CallbackContext ctx)
+        => OnKeyOnePress?.Invoke();
 
-        for (int i = 0; i < _shapeKeys.Count; i++)
-        {
-            var key = _shapeKeys[i];
-            bool isActive = i == index;
-            if (key.runTimeObject != null)
-                key.runTimeObject.SetActive(isActive);
-        }
-    }
+    private void SwitchTwoPerformed(InputAction.CallbackContext ctx)
+        => OnKeyTwoPress?.Invoke();
 
-    private void TryHitNote()
-    {
-        Debug.Log("HitNote");
-
-        NoteControl[] notes = FindObjectsByType<NoteControl>(FindObjectsSortMode.None);
-        if (notes.Length == 0) return;
-
-        float currentTime = SongManager.Instance.GetSongTime();
-
-        NoteControl closest = notes
-            .Where(n => !n.IsHit())
-            .OrderBy(n => Mathf.Abs(currentTime - n.GetTargetTime()))
-            .FirstOrDefault();
-
-        if (closest != null)
-        {
-            bool hit = closest.TryHit(_currentShapeKey.keyValue);
-
-            if (!hit)
-                SongManager.Instance.RegisterHit(new MissJudgement());
-        }
-    }
+    private void SwitchThreePerformed(InputAction.CallbackContext ctx)
+        => OnKeyThreePress?.Invoke();
 }
